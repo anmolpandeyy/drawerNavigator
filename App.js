9332c177ca8fb5898c30b31ceb56334ea1050b41
Anmol Pandey <anmol@classpro.in>
2018-10-29 19:30:25 +0530

    added drawer icon

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { createDrawerNavigator } from "react-navigation";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Dimensions,
+  Image
+} from "react-native";
+import { createDrawerNavigator, DrawerItems } from "react-navigation";
 import HomeScreen from "./screens/HomeScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 
@@ -10,9 +18,28 @@ class App extends Component {
   }
 }
 
-const AppDrawerNavigator = createDrawerNavigator({
-  Home: { screen: HomeScreen },
-  Settings: { screen: SettingsScreen }
-});
+const CustomDrawerComponent = props => (
+  <SafeAreaView style={{ flex: 1 }}>
+    <View style={{ alignItems: "center", justifyContent: "center" }}>
+      <Image
+        source={require("./assets/icon.png")}
+        style={{ height: 120, width: 120, borderRadius: 60 }}
+      />
+    </View>
+    <ScrollView>
+      <DrawerItems {...props} />
+    </ScrollView>
+  </SafeAreaView>
+);
+
+const AppDrawerNavigator = createDrawerNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Settings: { screen: SettingsScreen }
+  },
+  {
+    contentComponent: CustomDrawerComponent
+  }
+);
 
 export default App;
