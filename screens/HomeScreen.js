23a8bab12daa4103760b5802de5c82e8656c4f16
Anmol Pandey <anmol@classpro.in>
2018-10-30 13:57:13 +0530

    added bottomTabNavigator

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,43 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
-
+import Icon from "react-native-vector-icons/AntDesign";
+import { createBottomTabNavigator } from "react-navigation";
+import SettingsScreen from "./SettingsScreen";
 class HomeScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>HomeScreen</Text>
+        <Text>Home Screen</Text>
       </View>
     );
   }
 }
-export default HomeScreen;
+export default createBottomTabNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarLabel: "Home",
+      tabBarOptions: {
+        activeTintColor: "red"
+      },
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="home" color={tintColor} size={24} />
+      )
+    }
+  },
+  Profile: {
+    screen: SettingsScreen,
+    navigationOptions: {
+      tabBarLabel: "Profile",
+      tabBarOptions: {
+        activeTintColor: "red"
+      },
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="setting" color={tintColor} size={24} />
+      )
+    }
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
-
+import Icon from "react-native-vector-icons/AntDesign";
 class SettingsScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>SettingsScreen</Text>
+        <Text>Setting Screen</Text>
       </View>
     );
   }
